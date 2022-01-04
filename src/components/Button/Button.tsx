a8a2dbb7411516bf262fecb7ff74c0806e2f0f42
Afoejoe <afoejoe@gmail.com>
2022-01-04 20:37:17 +0300

    initial setup

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import './Button.css';
+
+import React, { FC } from 'react';
+
+interface ButtonProps {
+  label: string;
+}
+const Button: FC<ButtonProps> = ({ label }) => {
+  return <button>{label}</button>;
+};
+
+export { ButtonProps };
+export default Button;
